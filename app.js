4e6571bea3e57c6931d84f3a7dc1e4a73aeb960d
Jo hanbin <whgksqls9999@naver.com>
2023-10-13 00:19:43 +0900

    단순 Login 구현

--- app.js
+++ app.js
@@ -2,27 +2,38 @@ const loginForm = document.querySelector('#login-form');
 const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
-const HIDDEN_CLASSNAME = "hidden";
-const USERNAME_KEY = "username";
+const HIDDNE_CLASS = "hidden";
+const USERNAME = "username";
 
+// 1. onLoginSubmit 구현
 function onLoginSubmit(event){
+    // submit 기본 기능 실행 막기
     event.preventDefault();
-    loginForm.classList.add(HIDDEN_CLASSNAME);
+    // value 변수에 저장
     const username = loginInput.value;
+    // 로컬 저장소에 이름 저장
     localStorage.setItem("username",username);
+    // 로그인 폼 숨기기
+    loginForm.classList.add(HIDDNE_CLASS);
     paintGreeting(username);
 }
-
+// 2. paintGreeting 구현
 function paintGreeting(username){
+    greeting.classList.remove(HIDDNE_CLASS);
     greeting.innerText = `Hello! ${username}`;
-    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-const savedUsername = localStorage.getItem(USERNAME_KEY);
+// 3. 로그인 폼, greeting 페이지 출력 구현
+const savedUsername = localStorage.getItem("username");
 
+// 로컬 저장소에 이름이 저장되어 있지 않으면
 if (savedUsername == null){
-    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    // 로그인 폼을 보여주고 greeting은 안보여야 한다
+    loginForm.classList.remove(HIDDNE_CLASS);
     loginForm.addEventListener('submit',onLoginSubmit);
 } else {
+    // greeting을 보여주자
+    greeting.classList.remove(HIDDNE_CLASS);
     paintGreeting(savedUsername);
+
 }
